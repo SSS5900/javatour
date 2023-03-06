138b806d4e1bf8413cc7b3326b527bfa28e06137
SSS5900 <sosnovskihsergey@gmail.com>
2023-03-07 00:38:06 +0500

    task in src/main-2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-     int eaters = 5; // сколько людей бубут есть
+     int eaters = 5; // сколько людей будут есть
 
         int water = 3000; // миллилитров воды
         int potatoes = 5; // картофелин
